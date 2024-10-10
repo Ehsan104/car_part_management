b98881c9caf36535a4e9a69809195e45b7d614d8
Ehsan104 <Nehsan2120@gmail.com>
2024-10-10 13:11:19 -0700

    switching to using TinyDB, seems easier to implement

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
-from flask import Flask 
-import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
 
 #first lets initlize the database 
-def database():
-    conn = sqlite3.connect('parts.db')
-    c = conn.cursor()
-    conn.execute('CREATE TABLE parts (part_name TEXT, part_category TEXT, part_price TEXT, part_quantity TEXT, part_manufacturer TEXT)')
-    conn.commit()
-    conn.close()
-database()
+db = TinyDB('car_parts.json') #This will create a json file called car_parts.json (this will be considered the database)
 
 @app.route('/')
-    def index():
-        conn = sqlite3.connect('parts.db')
-        c = conn.cursor()
-        c.execute('SELECT * FROM parts')
-        parts = c.fetchall()
-        conn.close()
-        return render_template('index.html', parts=parts)
+def index():
+    parts = db.all() #This will get all the parts in the database
+    return render_template('index.html', parts=parts)`
 
 #This is where the all the functions will be defined
 
 @app.route('/add', methods=['POST'])
-def add_part():
-    part_name = request.form['part_name']
-    part_category = request.form['part_category']
-    part_price = request.form['part_price']
-    part_quantity = request.form['part_quantity']
-    part_manufacturer = request.form['part_manufacturer']
-    
-      
-
-
+def add_part(): 
+    #this function will add a part to the database
 
 @app.route('/delete', methods=['POST'])
+def delete_part():
+    #this function will delete a part from the database
+
+@app.route('/lookup', methods=['POST'])
+def lookup_part():
+    #this function will look up a part in the database
+
+@
